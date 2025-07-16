d12b0c47628849dc886deb6b088bf5b28177b6e0
Abel_Skaria2004 <123005471+Jake1902@users.noreply.github.com>
2025-07-16 16:45:44 +0530

    Fix onboarding compile errors

--- lib/features/onboarding/pages/theme_page.dart
+++ lib/features/onboarding/pages/theme_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
-import '../../core/data/preferences_service.dart';
+import '../../../core/data/preferences_service.dart';
 import '../onboarding_controller.dart';
 
 class ThemePage extends StatefulWidget {
